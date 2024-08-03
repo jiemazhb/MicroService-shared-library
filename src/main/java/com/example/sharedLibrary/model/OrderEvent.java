8dcf93e5851bbe763d54b6c886f63a7272947e4b
Danny Zhao <Danny.H.Zhao@gmail.com>
2024-08-03 14:56:45 -0700

    first commit

---
+++ src/main/java/com/example/sharedLibrary/model/OrderEvent.java
@@ -0,0 +1,14 @@
+package com.example.sharedLibrary.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class OrderEvent {
+    private long orderId;
+    private long productId;
+    private long quantity;
+    private String paymentMode;
+    private long totalAmount;
+}
